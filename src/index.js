10b968120de3020d938df84948dc7ee7ecfd64f6
ziad <hatereba@gmail.com>
2024-05-21 13:50:02 +0300

    hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 import {
   BrowserRouter,
   Routes,
@@ -9,10 +10,10 @@ import {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path = "/" element = {<App />}></Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
